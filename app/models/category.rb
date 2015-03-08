6a9d9ffdd89db5386a9d9e6076bcef64964e061e
Daniel Uribe <duribe8@gmail.com>
2015-03-08 15:21:22 -0700

    Adding code for safekeeping

---
+++ app/models/category.rb
@@ -0,0 +1,6 @@
+class Category < ActiveRecord::Base
+
+  has_and_belongs_to_many :articles
+
+  default_scope lambda { order('categories.name') }
+end
---
+++ db/migrate/20150301002330_rename_password_to_hashed_pasword.rb
@@ -0,0 +1,5 @@
+class RenamePasswordToHashedPasword < ActiveRecord::Migration
+  def change
+    rename_column :users, :password, :hashed_password
+  end
+end
